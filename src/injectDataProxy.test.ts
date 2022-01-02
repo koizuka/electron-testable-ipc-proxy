c840d09c449d01051960c8d3ef77b30acf01f5db
Akihiko Koizuka <koizuka@gmail.com>
2022-01-03 00:09:27 +0900

    add injectDataProxy

---
+++ src/injectDataProxy.test.ts
@@ -0,0 +1,85 @@
+import { injectDataReceiverProxy, injectDataSenderProxy } from "./injectDataProxy";
+import { IpcProxyDescriptor } from "./IpcProxyDescriptor";
+
+interface TestInterface {
+  doSomething: (param: number) => Promise<void>;
+  data: number | undefined;
+  promiseData: Promise<string>;
+  undefinedData: number | undefined;
+}
+
+class TestTemplate implements TestInterface {
+  private dontCallMe = new Error("don't call me");
+  private dummyPromise = new Promise(() => { }) as Promise<any>;
+
+  doSomething(): Promise<never> { throw this.dontCallMe; }
+  data: number | undefined = 42; // dummy value
+  promiseData = this.dummyPromise;
+  undefinedData: number | undefined;
+}
+
+const desc: IpcProxyDescriptor<TestInterface> = {
+  window: 'window',
+  IpcChannel: 'test',
+  template: new TestTemplate(),
+};
+
+class MockClass implements TestInterface {
+  async doSomething(n: number): Promise<void> { }
+  data: number | undefined = 1;
+  promiseData = Promise.resolve('hello');
+  undefinedData: number | undefined;
+}
+
+function isPromise(v: unknown): v is Promise<unknown> {
+  return v instanceof Promise;
+}
+
+test('injectDataReceiverProxy', async () => {
+  const object = new MockClass();
+  const { receiver, nextValue } = injectDataReceiverProxy(object, desc);
+
+  expect(object.hasOwnProperty('data')).toBe(false);
+  expect(object.data).toBe(undefined);
+  expect(object.promiseData).not.toBe(undefined);
+  expect(isPromise(object.promiseData)).toBe(true);
+
+  expect(object.promiseData).resolves.toBe('hello');
+
+  const p = nextValue('data');
+  receiver('data', 1);
+  expect(object.hasOwnProperty('data')).toBe(true);
+  expect(object.data).toBe(1);
+  expect(p).resolves.toBe(1);
+
+  // make sure data is read only
+  expect(() => { object.data = 3; }).toThrowError;
+});
+
+test('injectDataSenderProxy', async () => {
+  const sender = new MockClass();
+  sender.data = 1;
+  sender.promiseData = new Promise(() => { }); // never resolves
+
+  const receiver = jest.fn<void, [string, unknown]>();
+  injectDataSenderProxy(sender, receiver);
+
+  expect(receiver).toBeCalledWith('data', 1);
+  expect(sender.data).toBe(1);
+
+  sender.data = 2;
+  expect(receiver).toBeCalledWith('data', 2);
+  expect(sender.data).toBe(2);
+
+  sender.promiseData = Promise.resolve('good-bye');
+  await Promise.resolve();
+  expect(receiver).toBeCalledWith('promiseData', 'good-bye');
+
+  sender.undefinedData = 3;
+  expect(receiver).not.toBeCalledWith('undefinedData', 3);
+
+  sender.data = undefined;
+  expect(receiver).toBeCalledWith('data', undefined);
+  sender.data = 4;
+  expect(receiver).toBeCalledWith('data', 4);
+});
---
+++ src/injectDataProxy.ts
@@ -0,0 +1,141 @@
+import { IpcProxyDescriptor } from "./IpcProxyDescriptor";
+
+function isPromise(v: unknown): v is Promise<unknown> {
+  return v instanceof Promise;
+}
+
+/** inject getter/setter to data members(which value is NOT `undefined`) of the target.
+ *     - setter: store the value and call fn (expected to send the value via IPC).
+*      - getter: read the stored value.
+ *   call fn each data members(except Promise)'s initial value from this function.
+ *
+ *   NOTE:
+ *     if the value is yet undetermined when initialization time, use Promise rather than value `undefined`.
+ *     property initialized with `undefined` will not be injected proxy.
+ */
+export function injectDataSenderProxy<T extends {}>(target: T, fn: (key: keyof T, value: unknown) => void): void {
+  const keys = (Object.getOwnPropertyNames(target) as (keyof T)[]).filter(key => typeof target[key] !== 'function');
+  if (keys.length === 0) {
+    return;
+  }
+
+  const data: { [k in keyof T]?: unknown; } = {};
+
+  const defineKey = (key: keyof T, initialValue: unknown) => {
+    Object.defineProperty(target, key, {
+      set: (value) => {
+        data[key] = value;
+        if (isPromise(value)) {
+          value.then(v => {
+            fn(key, v);
+          });
+        } else {
+          fn(key, value);
+        }
+      },
+      get: () => data[key],
+      enumerable: true,
+    });
+
+    data[key] = initialValue;
+    if (!isPromise(initialValue) && initialValue !== undefined) {
+      fn(key, initialValue);
+    }
+  };
+
+  for (const key of keys) {
+    defineKey(key, target[key]);
+  }
+}
+
+/** receiver:
+ *   - data member: read only
+ *     - Promise: resolve when initial value has received
+ *     - other: `undefined` before initial value has received
+ * 
+ *   - receiver(key, value): call when value has received. data member is set to the value
+ *   - nextValue(key): resolve when next value has received
+ */
+export function injectDataReceiverProxy<T extends {}>(target: T, desc: IpcProxyDescriptor<T>): {
+  receiver: (key: keyof T, value: unknown) => void,
+  nextValue: (key: keyof T) => Promise<unknown>,
+} {
+  const from: T = desc.template;
+  const keys = (Object.getOwnPropertyNames(from) as (keyof T)[]).filter(key => typeof from[key] !== 'function');
+
+  for (const key of keys) {
+    if (target.hasOwnProperty(key)) {
+      delete target[key];
+    }
+  }
+
+  const data: { [k in keyof T]: {
+    value?: unknown;
+    promise: Promise<unknown> | null;
+    resolver: ((value: unknown) => void) | null;
+  } } =
+    keys.reduce(
+      (prev, key) => {
+        prev[key] = {
+          promise: null,
+          resolver: null,
+        };
+        return prev;
+      },
+      {} as { [k in keyof T]: {
+        value?: unknown;
+        promise: Promise<unknown> | null;
+        resolver: ((value: unknown) => void) | null;
+      } },
+    );
+
+  const nextValue = (key: keyof T) => {
+    if (data[key].promise === null) {
+      data[key].promise = new Promise((resolve) => {
+        data[key].resolver = resolve;
+      });
+    }
+    return data[key].promise as Promise<unknown>;
+  };
+
+  for (const key of keys) {
+    if (isPromise(from[key])) {
+      Object.defineProperty(target, key, {
+        get: () => nextValue(key),
+      });
+    }
+  }
+
+  return {
+    receiver: (key: keyof T, value: unknown) => {
+      if (!data.hasOwnProperty(key)) {
+        throw new Error(`unknown key: ${key}`);
+      }
+      if (typeof from[key] === 'function') {
+        throw new Error(`key is function: ${key}`);
+      }
+
+      if (!target.hasOwnProperty(key)) {
+        Object.defineProperty(target, key, {
+          get: () => {
+            if (!data.hasOwnProperty(key) || !data[key].hasOwnProperty('value')) {
+              return undefined; // not yet received
+            }
+            const d = data[key] as { value: unknown };
+            return d.value;
+          }
+        });
+      }
+
+      data[key].value = value;
+      const resolver = data[key].resolver;
+      if (resolver !== null) {
+        resolver(value);
+        data[key].resolver = null;
+        data[key].promise = null;
+      }
+    },
+
+    nextValue,
+  };
+}
